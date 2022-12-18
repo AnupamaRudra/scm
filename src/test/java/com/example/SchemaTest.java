64c5ab0263716bbea1d206f0462c53cb50d2d8c2
AnupamaRudra <anupamarudra111@gmail.com>
2022-12-19 00:05:26 +0530

    Adding tests

--- src/test/java/com/example/SchemaTest.java
+++ src/test/java/com/example/SchemaTest.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -14,12 +15,28 @@ public class SchemaTest {
 
     @Test
     public void testChanges() {
+        Assertions.assertTrue(verifyChanges());
+    }
+
+    private boolean verifyChanges() {
         try {
             lines =
                     Files.readAllLines(Paths.get("target/changes.txt"), StandardCharsets.UTF_8);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        int z=8;
+        for (String line : lines) {
+            String current = line.replaceAll("\\s", "");
+            if (current.length() > 1 && current.charAt(0) == '-' && current.charAt(1) != '-') {
+                if (current.lastIndexOf("title") == 2) {
+                    continue;
+                }
+                if (current.lastIndexOf("description") == 2) {
+                    continue;
+                }
+                return false;
+            }
+        }
+        return true;
     }
 }
